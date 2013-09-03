7677581d163f6753d2b24b50f307588de2c85077
Alexandru Lixandru <alex.lixandru@gmail.com>
2013-09-03 19:25:21 +0300

    Updated plugin version

--- qa-plugin.php
+++ qa-plugin.php
@@ -29,8 +29,8 @@
 	Plugin Name: Open Login
 	Plugin URI: https://github.com/alixandru/q2a-open-login
 	Plugin Description: Allows users to log in via Facebook, Google and other Open Auth providers
-	Plugin Version: 2.0.1
-	Plugin Date: 2013-08-14
+	Plugin Version: 2.0.2
+	Plugin Date: 2013-09-03
 	Plugin Author: Alex Lixandru
 	Plugin Author URI: http://alex.punctsivirgula.ro/
 	Plugin License: GPLv2
