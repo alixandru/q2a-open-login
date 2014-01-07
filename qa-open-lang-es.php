937979a51f9122c044fbaeeaf6fd832508b5bafe
jorpcolombia <jorpcolombia@gmail.com>
2014-01-06 21:22:12 -0500

    Add spanish translations

---
+++ qa-open-lang-es.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+	Question2Answer (c) Gideon Greenspan
+	Open Login Plugin (c) Alex Lixandru
+
+	http://www.question2answer.org/
+
+	
+	File: qa-plugin/open-login/qa-open-lang-es.php
+	Version: 1.0.0
+	Description: Spanish translation of all plugin texts
+
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+return array(
+	'my_logins_title' => 'Mis inicios de sesión',
+	'my_logins_nav' => 'Mis inicios de sesión',
+	'my_current_user' => 'Mi cuenta actual',
+	'associated_logins' => 'Cuentas conectadas',
+	'other_logins' => 'Otras cuentas con la misma dirección de correo',
+	'other_logins_conf_title' => 'Confirma las cuentas conectadas',
+	'other_logins_conf_text' => 'Hemos detectado que hay otras cuentas usando la misma dirección de correo. Si éstas cuentas te pertenecen puedes conectarlas con la cuenta actual para evitar duplicados.',
+	'merge_accounts' => 'Conectar las cuentas seleccionadas',
+	'merge_accounts_note' => '¡Importante! Las cuentas seleccionadas serán conectadas con tu cuenta actual y su perfil original será borrado permanentemente. Los puntos y la actividad previa no se podrá conectar, así que aparecerá como anónima. Si deseas en cambio mantener una de estas cuentas y borrar la actual, inicia sesión con esa otra cuenta y visita ésta página de nuevo.',
+	'split_accounts' => 'Desconectar las cuentas seleccionadas',
+	'split_accounts_note' => 'Nota: una vez desconectadas, tu perfil no podrá ser asociado con esas cuentas. Si inicias sesión con las cuentas después de desconectarlas, se les crearan nuevos perfiles. Puedes, sin embargo, volver a conectar esas cuentas ingresando en esta página.',
+	'no_logins_title' => 'No hay más cuentas conectadas para tu perfil',
+	'no_logins_text' => 'Inicia sesión con un proveedor OpenID para conectar tu perfil actual con otras cuentas.',
+	'local_user' => 'Cuenta de usuario local',
+	'login_title' => 'Inicio de sesión con un proveedor externo',
+	'login_description' => 'Escoge un proveedor de la siguiente lista para iniciar sesión sin crear una cuenta en este sitio.',
+	'remember_me' => 'No cerrar sesión cuando inicio sesión usando una de las cuentas conectadas.',
+	'login_using' => 'Entrar con ^',
+);
