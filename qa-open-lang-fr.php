484adf33344eeab5e0d78d3af9c0853a4d4104fc
Alain Beauvois <alain@itipic.com>
2014-07-07 11:17:45 +0000

    Add french translation

---
+++ qa-open-lang-fr.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+	Question2Answer (c) Gideon Greenspan
+	Open Login Plugin (c) Alex Lixandru
+
+	http://www.question2answer.org/
+
+	
+	File: qa-plugin/open-login/qa-open-lang-default.php
+	Version: 2.0.0
+	Description: Default English translation of all plugin texts
+
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+
+return array(
+	'my_logins_title' => 'Mes modes d\'authentification',
+	'my_logins_nav' => 'Mes modes d\'authentification',
+	'my_current_user' => 'Mon compte actuel',
+	'associated_logins' => 'Mes autres mode authentifications',
+	'other_logins' => 'Autres comptes correspondants à cette adresse email',
+	'other_logins_conf_title' => 'Confirmez l\'association des comptes',
+	'other_logins_conf_text' => 'Nous avons détectés que d\'autres comptes-utilisateur utilisent la même adresse email. Si ces comptes vous appartiennent, vous pouvez les associer à votre profil actuel pour évitez les duplications.',
+	'merge_accounts' => 'Associer les comptes-utilisateur sélectionnés',
+	'merge_accounts_note' => 'Important! Les modes d\'authentification sélectionnés seront associés à votre profil courant et leur profil original seront définitivement supprimés. Les points de réputation appartenant à ces profils ne seront pas récupérés, et toutes les activités précédentes seront marquées comme anonnonymes. Si vous préférez garder l\'un des autres comptes et supprimer celui que vous utilsez actuellement, identifiez-vous avec cet autre compte et visitez à nouveau cette page.',
+	'split_accounts' => 'Dissocier les comptes sélectionnés',
+	'split_accounts_note' => 'Note: une fois dissocié, votre profil ne sera plus associé avec ces comptes. Si vous vous authentifiez à nouveau avec l\'un des identifiants de ces comptes, un nouveau compte sera créé. Vous pouvez néanmoins, les fusionner à nouveau avec ce profil en visitant cette page.',
+	'no_logins_title' => 'Aucun autre mode d\'authentification lié à votre profil',
+	'no_logins_text' => 'Identifiez-vous à l\'aide d\'un service d\'authentification externe pour l\'associer à votre compte actuel.',
+	'local_user' => 'Compte-utilisateur local',
+	'login_title' => 'Identification à l\'aide d\'un service d\'authentification externe',
+	'login_description' => 'Choisissez un mode d\'authentification dans la liste ci-dessous pour vous identifier sans créer un compte-utilisateur sur ce site.',
+	'remember_me' => 'Gardez-moi connecté quand je m\'identifie à l\'aide d\'un mode d\'authentification lié à mon compte',
+	'login_using' => 'S\'authentifier avec  ^'
+);
